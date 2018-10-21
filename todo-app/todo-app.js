dd9c85204e602ff6678cc938c643d7544e3fcd6e
Ray Taylor <ray.taylor@asg-worldwide.com>
2018-10-21 21:33:46 +0100

    Add filtering to to do app.

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,6 +15,10 @@ const toDos =[{
     completed: false
 }]
 
+filters = {
+    searchText: ''
+}
+
 // create toDosIncomplete with all toDos that are completed: false
 const toDosIncomplete = toDos.filter(function (toDo) {
     return !toDo.completed
@@ -26,13 +30,24 @@ summary.textContent = `You have ${toDosIncomplete.length} things to do!`
 document.querySelector('body').appendChild(summary)
 
 // write out all to dos
-toDos.forEach(function (toDo) {
-    const p = document.createElement('p')
-    p.textContent = toDo.text
-    document.querySelector('body').appendChild(p)
-})
+// const renderToDos = function (toDos, filters) {
+//     const filterToDos = toDos.toLowerCase().includes(filters.searchText.toLowerCase())
+
+//     filterToDos.forEach(function (toDo) {
+//     const p = document.createElement('p')
+//     p.textContent = toDo.text
+//     document.querySelector('#todos').appendChild(p)
+// })
+
+// renderToDos(toDos)
 
 // listen to "Add New To Do" click
 document.querySelector('#add-todo').addEventListener('click', function (e) {
     console.log('"Add New To Do" button was clicked')
+})
+
+// listen to Input
+document.querySelector('#input-todo').addEventListener('input', function(e) {
+    filters.searchText = e.target.value
+    console.log(filters)
 })
