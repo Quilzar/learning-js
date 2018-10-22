e6ed7adb5157bb83a437994b43f8016cdb83e54b
Ray Taylor <quilzar@me.com>
2018-10-22 10:58:23 +0100

    End of section 6.

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,3 +1,4 @@
+// create to dos object
 const toDos =[{
     text: 'Play Dinosaur Island',
     completed: true
@@ -15,39 +16,78 @@ const toDos =[{
     completed: false
 }]
 
-filters = {
-    searchText: ''
+// create object to store filters
+const filters = {
+    searchText: '',
+    hideCompleted: false
 }
 
-// create toDosIncomplete with all toDos that are completed: false
-const toDosIncomplete = toDos.filter(function (toDo) {
-    return !toDo.completed
-})
+// filter and render to dos
+const renderToDos = function (toDos, filters) {
+    // create filtered to dos
+    const filteredToDos = toDos.filter(function (toDo) {
+        const searchTextMatch = toDo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompletedMatch = !filters.hideCompleted || !toDo.completed
 
-// write out summary
-const summary = document.createElement('h2')
-summary.textContent = `You have ${toDosIncomplete.length} things to do!`
-document.querySelector('body').appendChild(summary)
+        return searchTextMatch && hideCompletedMatch
+    })
 
-// write out all to dos
-// const renderToDos = function (toDos, filters) {
-//     const filterToDos = toDos.toLowerCase().includes(filters.searchText.toLowerCase())
+    // create toDosIncomplete with all toDos that are completed: false
+    const toDosIncomplete = filteredToDos.filter(function (toDo) {
+        return !toDo.completed
+    })
 
-//     filterToDos.forEach(function (toDo) {
-//     const p = document.createElement('p')
-//     p.textContent = toDo.text
-//     document.querySelector('#todos').appendChild(p)
-// })
+    // clear out div tag
+    document.querySelector('#to-dos').innerHTML = ''
 
-// renderToDos(toDos)
+    // write out summary to #to-dos div tag
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${toDosIncomplete.length} things left to do!`
+    document.querySelector('#to-dos').appendChild(summary)
+    
+    // write out to dos to #to-dos div tag
+    filteredToDos.forEach(function (toDo) {
+        const p = document.createElement('p')
+        p.textContent = `${toDo.text} (${toDo.completed})` 
+        document.querySelector('#to-dos').appendChild(p)
+    })
+}
 
-// listen to "Add New To Do" click
-document.querySelector('#add-todo').addEventListener('click', function (e) {
-    console.log('"Add New To Do" button was clicked')
-})
+// initial render to do <div>
+renderToDos(toDos, filters)
 
-// listen to Input
-document.querySelector('#input-todo').addEventListener('input', function(e) {
+// listen to #filter-todo
+document.querySelector('#filter-todo').addEventListener('input', function(e) {
+    // update filters object
     filters.searchText = e.target.value
-    console.log(filters)
+
+    // rerender to-dos <div>
+    renderToDos(toDos, filters)
+})
+
+// listen to #add-todo
+document.querySelector('#add-todo').addEventListener('submit', function(e) {
+    // prevent default behaviour (avoid refresh)
+    e.preventDefault()
+
+    // add sumbited to do to toDos object
+    toDos.push({
+        text: e.target.elements.newToDo.value,
+        completed: false
+    })
+
+    // rerender to-dos <div>
+    renderToDos(toDos, filters)
+
+    // clear input form
+    e.target.elements.newToDo.value = ''
+})
+
+// listen to #hide-completed
+document.querySelector('#hide-completed').addEventListener('change', function(e) {
+    // update filters object
+    filters.hideCompleted = e.target.checked
+
+    // rerender to-dos <div>
+    renderToDos(toDos, filters)
 })
